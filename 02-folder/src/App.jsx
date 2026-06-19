8605c4dffaaaf78d851097f24b074ce456d2bcbf
45GauravUpadhayay <45upadhayaygaurav264@gmail.com>
2026-06-19 15:05:16 +0530

    Components Create

--- 02-folder/src/App.jsx
+++ 02-folder/src/App.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
+import Card from './components/Card'
+import Navbar from './components/Navbar'
 
 const App = () => {
   return (
     <div>
-      <div className='card'>
-        <h1>Hello Gaurav Upadhayay</h1>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi, sint!</p>
-      </div>
+      <Navbar />
+      <Card />
+
     </div>
   )
 }
---
+++ 02-folder/src/components/Card.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Card = () => {
+  return (
+    <div className='Card'>
+      <h1>Gaurav Upadhayay</h1>
+      <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Libero, possimus!</p>
+    </div>
+  )
+}
+
+export default Card
