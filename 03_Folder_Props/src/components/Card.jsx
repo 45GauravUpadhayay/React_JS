52dc0eff3dcb1631180cdd19af0edfa7b09d8ef2
45GauravUpadhayay <45upadhayaygaurav264@gmail.com>
2026-06-19 15:46:48 +0530

    Card Created

---
+++ 03_Folder_Props/src/components/Card.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Card = () => {
+  return (
+      <div className="card">
+        <img src="https://plus.unsplash.com/premium_photo-1781215910967-7c7ab2e8cada?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" />
+        <h1>Gaurav Upadhayay</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+        <button>view profile</button>
+      </div>
+  )
+}
+
+export default Card
