67fa4c754f683acf78da3b8413b41d18f37c12b7
45GauravUpadhayay <45upadhayaygaurav264@gmail.com>
2026-06-20 14:56:11 +0530

    Basic Props Done

--- 03_Folder_Props/src/App.jsx
+++ 03_Folder_Props/src/App.jsx
@@ -4,8 +4,8 @@ import Card from './components/Card'
 const App = () => {
   return (
     <div className="parent">
-      <Card user = "Vivek" para = "Lorem ipsum dolor sit amet consect" />
-      <Card user = "Karan" para = " sit amet consectetur adipisicing elit." />
+      <Card user = "Vivek" para = "Lorem ipsum dolor sit amet consect" img = "https://images.unsplash.com/photo-1781772450980-65bd0a9bb6c8?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwyMXx8fGVufDB8fHx8fA%3D%3D" />
+      <Card user = "Karan" para = " sit amet consectetur adipisicing elit." img = "https://plus.unsplash.com/premium_photo-1688572454849-4348982edf7d?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHw2fHx8ZW58MHx8fHx8" />
     </div>
   )
 }
--- 03_Folder_Props/src/components/Card.jsx
+++ 03_Folder_Props/src/components/Card.jsx
@@ -7,7 +7,7 @@ const Card = (Props) => {
 
   return (
       <div className="card">
-        <img src="https://plus.unsplash.com/premium_photo-1781215910967-7c7ab2e8cada?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" />
+        <img src={Props.img}/>
         <h1>{Props.user}</h1>
         <p>{Props.para}</p>
         <button>view profile</button>
