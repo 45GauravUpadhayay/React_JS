74de7923ce0add151d2fd26e417882d28bf7d508
45GauravUpadhayay <45upadhayaygaurav264@gmail.com>
2026-06-28 15:31:16 +0530

    Button

---
+++ 07_CSS/src/components/Button/Button.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import "../style/Button.css"
+
+const Button = () => {
+  return (
+    <div className='button'>
+      This is a Button
+    </div>
+  )
+}
+
+export default Button
