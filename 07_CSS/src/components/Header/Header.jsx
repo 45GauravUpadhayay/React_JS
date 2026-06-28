9a123b59928d23ffdf447b7ee79a4f0d90b9d9c8
45GauravUpadhayay <45upadhayaygaurav264@gmail.com>
2026-06-28 15:31:58 +0530

    Header JSX

---
+++ 07_CSS/src/components/Header/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import "../style/Header.css"
+
+const Header = () => {
+  return (
+    <div className='header'>
+      <h3>Chai aur Code</h3>
+      <button>Login</button>
+    </div>
+  )
+}
+
+export default Header
