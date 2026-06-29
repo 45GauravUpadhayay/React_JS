398dabcef2418cfdd7026b26d1985b129536f188
45GauravUpadhayay <45upadhayaygaurav264@gmail.com>
2026-06-29 21:06:23 +0530

    Right Card

---
+++ 09_UI_project/src/components/Section1/HeroText.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const HeroText = () => {
+  return (
+    <div>
+      <div className="p-6">
+        <h3 className="mb-7 text-7xl font-bold leading-[1.1]">
+          Prospective <br /> <span className="text-gray-600">customer</span> <br /> Segmentation
+        </h3>
+        <p className="text-xl font-medium text-gray-600">
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Soluta accusamus aspernatur quos suscipit id! Blanditiis dolore recusandae illo harum esse.
+        </p>
+      </div>
+    </div>
+  )
+}
+
+export default HeroText
---
+++ 09_UI_project/src/components/Section1/Page1Content.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import LeftContent from './LeftContent'
+import RightContent from './RightContent'
+
+const Page1Content = () => {
+  return (
+    <div className='py-10 h-[87vh] gap-10 flex  px-18'>
+      <LeftContent />
+      <RightContent />
+    </div>
+  )
+}
+
+export default Page1Content
